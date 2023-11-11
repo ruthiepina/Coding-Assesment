b60b06dbef8f224a9e552da8ba88f6ba8ca4a205
RMP <ruthiepina@gmail.com>
2023-11-11 16:59:57 -0700

    Removed temporary hidden tags from index.html, added JS pseudo code and initialized screen.

---
+++ script.js
@@ -0,0 +1,27 @@
+function clearScreen() {
+   document.getElementById("heading").style.visibility = "hidden";
+   document.getElementById("quiz-intro").style.visibility = "hidden";
+   document.getElementById("start-quiz-btn").style.visibility = "hidden";
+
+   document.getElementById("question").style.visibility = "hidden";
+   document.getElementById("answer-buttons").style.visibility = "hidden";
+   document.getElementById("grade").style.visibility = "hidden";
+
+   document.getElementById("all-done-heading").style.visibility = "hidden";
+   document.getElementById("final-score").style.visibility = "hidden";
+   document.getElementById("initials-label").style.visibility = "hidden";
+   document.getElementById("initials").style.visibility = "hidden";
+   document.getElementById("score-submit").style.visibility = "hidden";
+
+   document.getElementById("high-scores-heading").style.visibility = "hidden";
+   document.getElementById("scores-list").style.visibility = "hidden";
+   document.getElementById("go-back-btn").style.visibility = "hidden";
+   document.getElementById("clear-btn").style.visibility = "hidden";
+}
+
+// TODO Initialize/clear screen
+clearScreen();
+// TODO Display intro to quiz page
+// TODO  Process start quiz button - display quiz page
+// TODO Display quiz page until all questions are processed or timed out
+// TODO Display All Done screen
