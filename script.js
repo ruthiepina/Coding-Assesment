c43b465a97f58922a3309b9f8cff623be956677f
RMP <ruthiepina@gmail.com>
2023-11-13 00:03:27 -0700

    Changed data types, fixed timer variable scope, displayed initial question, added listener for question grading, and added function to grade question.

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 var questionSet = [
    {
+      question: "Which method selects the 1st matching element in the document?",
+      answers: [
+         "document.querySelectorAll()",
+         "document.getElementsByClass()",
+         "document.querySelector()",
+         "document.getElementsByTagName()",
+      ],
+      correctAnsIndex: "2",
+   },
+   {
       question: "What statement do you use to exit or end a loop?",
       answers: ["Break statement", "Falter statement", "Close statement", "Conditional statement"],
-      correctAnsIndex: 0,
+      correctAnsIndex: "0",
    },
    {
       question: "Commonly used data types do NOT include:",
       answers: ["Strings", "Alerts", "Booleans", "Numbers"],
-      correctAnsIndex: 2,
+      correctAnsIndex: "2",
    },
    {
       question: "Arrays in JavaScript can be used to store:",
       answers: ["Numbers and strings", "Booleans", "Other arrays", "All of the above"],
-      correctAnsIndex: 3,
+      correctAnsIndex: "3",
    },
    {
       question: "What data type(s) are assigned to numeric values?",
       answers: ["Number", "Decimal", "Float", "Integer"],
-      correctAnsIndex: 0,
+      correctAnsIndex: "0",
    },
    {
       question: "Name the index of the item 'pineapple' in this array: ['grape', 'pineapple', 'banana', 'kiwi']",
       answers: ["0", "2", "1", "3"],
-      correctAnsIndex: 2,
+      correctAnsIndex: "2",
    },
    {
       question: "If let a = {}; and let b = [];, which statement is true:",
@@ -32,32 +42,22 @@ var questionSet = [
          "a is an Array and b is an Object",
          "a is undefined and b is also undefined",
       ],
-      correctAnsIndex: 1,
+      correctAnsIndex: "1",
    },
    {
       question: "What command is used to remove the last item in an array?",
       answers: ["splice()", "join()", "shift()", "pop()"],
-      correctAnsIndex: 3,
+      correctAnsIndex: "3",
    },
    {
       question: "What command sends data back to the code that called the function?",
       answers: ["return", "dispatch", "update", "respond"],
-      correctAnsIndex: 0,
+      correctAnsIndex: "0",
    },
    {
       question: "Every node in the DOM represents what?",
       answers: ["a link", "an HTML element", "a section", "a view"],
-      correctAnsIndex: 1,
-   },
-   {
-      question: "Which method selects the 1st matching element in the document?",
-      answers: [
-         "document.querySelectorAll()",
-         "document.getElementsByClass()",
-         "document.querySelector()",
-         "document.getElementsByTagName()",
-      ],
-      correctAnsIndex: 2,
+      correctAnsIndex: "1",
    },
 ];
 {
@@ -118,13 +118,36 @@ var timeLeft = 100;
 var startQuizBtn = document.getElementById("start-quiz-btn");
 startQuizBtn.addEventListener("click", startQuiz);
 
+var questionIndex = 0;
+var timeInterval = setInterval(myTimer, 1000);
+
 //* Process start quiz btn
 function startQuiz() {
    hideShowIntro("hide");
    hideShowQuestions("show");
 
-   for (var i = 0; i < questionSet.length; i++) {
-      displayQuestion(i);
+   console.log(questionSet);
+
+   displayQuestion(questionIndex);
+
+   //* Gets parent element of answer buttons - "bubbling"
+   var questionAnswerEl = document.getElementById("answer-buttons");
+   //* Adds event listener to parent element of answer buttons on click
+   //* with event handler gradeQuestion
+   questionAnswerEl.addEventListener("click", gradeQuestion);
+}
+
+//* Grades question answer as correct or wrong
+function gradeQuestion(evt) {
+   //* Gives us portion of the button element id that is an index to the button
+   //* that was clicked on
+   var answerId = evt.target.id; //* Gets the id of the child element that was clicked on - btn-x
+   var answerIndex = answerId.substring(4, 5); //* Removes "btn-" and gets index (0-3) only
+   console.log(answerIndex, "  ", questionSet[questionIndex].correctAnsIndex);
+   if (answerIndex === questionSet[questionIndex].correctAnsIndex) {
+      document.getElementById("grade").textContent = "Correct!";
+   } else {
+      document.getElementById("grade").textContent = "Wrong...";
    }
 }
 
@@ -151,7 +174,6 @@ function myTimer() {
       clearInterval(timeInterval); //* Use 'clearInterval()' to stop the timer
    }
 }
-var timeInterval = setInterval(myTimer, 1000);
 
 // TODO Display quiz page until all questions are processed or timed out
 // TODO Display All Done screen
